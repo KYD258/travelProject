73076ca585f834c9e96b9ee986a49f33221af86c
Administrator <905311607@qq.com>
2019-10-05 14:07:22 +0800

    后台订单部分，其中对tbUser表加了JPA所用注解以外，其他都未动，controller未避免繁杂，重新加了Mapping（"/sos"），可能对你们的业务有影响，你们根据自身情况进行修改

--- src/main/java/com/qf/domain/TbUser.java
+++ src/main/java/com/qf/domain/TbUser.java
@@ -3,12 +3,19 @@ package com.qf.domain;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.*;
 import java.util.Date;
-
 @Data
+@Entity
+@Table(name = "tb_user")
 public class TbUser {
+    @Id
+    @GeneratedValue(strategy =GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Integer userId;
+    @Column(name = "login_name")
     private String loginName;
+    @Column(name = "real_name")
     private String realName;
     private Integer sex;
     private Integer age;
@@ -16,6 +23,7 @@ public class TbUser {
     private String password;
     private String phone;
     private String email;
+    @Column(name = "create_time")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date createTime;
     private String pic;
